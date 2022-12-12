dc4bd64c17db363eef34ca8765ba866e9328e2fc
Michal Charemza <michal@charemza.name>
2022-12-12 19:56:38 +0000

    tests: include running on another repo

--- test_mirror_git_to_s3.py
+++ test_mirror_git_to_s3.py
@@ -3,3 +3,4 @@ from mirror_git_to_s3 import mirror_repos
 
 def test():
 	mirror_repos('https://github.com/uktrade/mirror-git-to-s3.git')
+	mirror_repos('https://github.com/uktrade/data-workspace.git')
