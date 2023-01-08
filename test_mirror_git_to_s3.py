00ba448d69292c73535599cc8d6117f6c874fc6b
Michal Charemza <michal@charemza.name>
2023-01-08 12:39:13 +0000

    tests: use same bucket in tests
    This is so later we can make sure the bucket is public read before we even get
    to the tests

--- test_mirror_git_to_s3.py
+++ test_mirror_git_to_s3.py
@@ -1,11 +1,12 @@
 import uuid
 import boto3
+import botocore
 
 from mirror_git_to_s3 import mirror_repos
 
 
 def test():
-    bucket_name = uuid.uuid4().hex
+    bucket_name = 'my-bucket'
 
     s3_client = boto3.client('s3',
         endpoint_url='http://127.0.0.1:9000/',
@@ -13,7 +14,18 @@ def test():
         aws_secret_access_key='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
         region_name='us-east-1',
     )
-    s3_client.create_bucket(Bucket=bucket_name)
+    try:
+        s3_client.create_bucket(Bucket=bucket_name)
+    except s3_client.exceptions.BucketAlreadyOwnedByYou:
+        pass
+    paginator = s3_client.get_paginator('list_objects_v2')
+    for page in paginator.paginate(Bucket=bucket_name):
+        items = [
+            {'Key': item['Key']}
+            for item in page.get('Contents', [])
+        ]
+        if items:
+            s3_client.delete_objects(Bucket=bucket_name, Delete={'Objects': items})
 
     mirror_repos((
         ('https://github.com/uktrade/mirror-git-to-s3.git', f's3://{bucket_name}/mirror-git-to-s3'),
